4cff9194ac7dd2d36b2a4de040aefb4541903a50
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-10-10 21:58:01 +0700

    Lab2 V2

---
+++ lab2/v2.lisp
@@ -0,0 +1,14 @@
+;; чтобы передать в my-all функцию по имени,
+;; используйте #', как в примере
+;; all от пустого списка равно t
+(defun my-all (p x)
+  (if (null x)
+      t
+      (if (not (funcall p (car x)))
+	  nil
+	  (all p (cdr x)))))
+
+(defun my-p (x) (= x 3))
+
+(pprint (my-all #'my-p '(3 3 3 3 3 3 3))) ; T
+(pprint (my-all #'my-p '(3 3 3 2 3 3 3))) ; NIL
